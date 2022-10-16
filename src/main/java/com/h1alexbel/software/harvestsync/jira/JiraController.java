ea13e71a224d88ce4138722ab813da6defe9d9c4
Aliaksei <abialiauski.dev@gmail.com>
2022-10-16 18:49:39 +0300

    fix(token): test token updated;

--- src/main/java/com/h1alexbel/software/harvestsync/jira/JiraController.java
+++ src/main/java/com/h1alexbel/software/harvestsync/jira/JiraController.java
@@ -1,11 +1,15 @@
 package com.h1alexbel.software.harvestsync.jira;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 /**
  * @author Aliaksei Bialiauski (abialiauski@sovld.com)
@@ -16,15 +20,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class JiraController {
 
-  private final JiraRestClient restClient;
+  //  private final JiraRestClient restClient;
+  @Value("${jira.domain}")
+  private String domain;
+  @Value("${jira.username}")
+  private String username;
 
-  @GetMapping("/spent/{id}")
-  public Integer getSpentMinutesByIssueKey(@PathVariable String id) {
-    return this.restClient
-        .getIssueClient()
-        .getIssue(id)
-        .claim()
-        .getTimeTracking()
-        .getTimeSpentMinutes();
+  @GetMapping("/{token}/spent/{id}")
+  public Integer getSpentMinutesByIssueKey(@PathVariable String token, @PathVariable String id) {
+//    return this.restClient
+//        .getIssueClient()
+//        .getIssue(id)
+//        .claim()
+//        .getTimeTracking()
+//        .getTimeSpentMinutes();
+    JiraRestClient restClient = new AsynchronousJiraRestClientFactory()
+        .createWithBasicHttpAuthentication(URI.create(this.domain),
+            this.username, token
+        );
+    return restClient.getIssueClient()
+        .getIssue(id).claim().getTimeTracking().getTimeSpentMinutes();
   }
+
+//  @Bean
+//  public JiraRestClient restClient1(String token) {
+//    return new AsynchronousJiraRestClientFactory()
+//        .createWithBasicHttpAuthentication(URI.create(this.domain),
+//            this.username, token
+//        );
+//  }
 }
