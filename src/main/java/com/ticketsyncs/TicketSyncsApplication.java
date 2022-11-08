da2c2767cd0fd8430107a8c9e30a495b50015ad2
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-08 15:21:32 +0300

    updated pom.xml;

---
+++ src/main/java/com/ticketsyncs/TicketSyncsApplication.java
@@ -0,0 +1,12 @@
+package com.ticketsyncs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSyncsApplication {
+
+  public static void main(final String... args) {
+    SpringApplication.run(TicketSyncsApplication.class, args);
+  }
+}
