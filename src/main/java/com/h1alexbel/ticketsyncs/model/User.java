c012b428ef1b00f5cf02999dc51d0f63d8609515
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-15 14:40:09 +0300

    removed docs;

--- src/main/java/com/h1alexbel/ticketsyncs/model/User.java
+++ src/main/java/com/h1alexbel/ticketsyncs/model/User.java
@@ -4,9 +4,5 @@ package com.h1alexbel.ticketsyncs.model;
  * @author Aliaksei Bialiauski (abialiauski@solvd.com)
  * @since 1.0
  */
-public interface SpentTimeInHours {
-
-  default Double fromMinutes(final Integer minutes) {
-    return 0.0;
-  }
+public interface User {
 }
